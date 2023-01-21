0d38ee6323b61b8291cde13756e89c6736498488
ysk <103978285+infinitumwrld@users.noreply.github.com>
2023-01-21 16:25:20 -0500

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
                         quantity: item.quantity
                     }
                 }),
-                success_url: `${req.headers.origin}/?success`,
-                cancel_url: `${req.headers.origin}/?canceled`,
+                success_url: `${req.headers.origin}/success`,
+                cancel_url: `${req.headers.origin}/canceled`,
               }
 
             // Create Checkout Sessions from body params.
